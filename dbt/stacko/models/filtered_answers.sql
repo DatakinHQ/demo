b1e389c8a4ec370585ed09b4f5735a4e730f94fd
Ross Turk <ross@rossturk.com>
2021-10-20 01:27:05 -0400

    we dont really need to be filtering by date here
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- dbt/stacko/models/filtered_answers.sql
+++ dbt/stacko/models/filtered_answers.sql
@@ -7,9 +7,6 @@
 select * from {{ source('stackoverflow', 'posts_answers') }}
 where parent_id in (select id from {{ ref('filtered_questions') }} )
 
--- to keep things from getting out of hand!
-and creation_date > '2021-01-01'
-
 -- only grab the rows we don't have
 {% if is_incremental() %}
   and creation_date > (select max(creation_date) from {{ this }})
--- dbt/stacko/models/filtered_users.sql
+++ dbt/stacko/models/filtered_users.sql
@@ -10,9 +10,6 @@ where (
     or id in (select owner_user_id from {{ ref('filtered_answers') }} )
 )
 
--- to keep things from getting out of hand!
-and creation_date > '2021-01-01'
-
 -- only grab the rows we don't have
 {% if is_incremental() %}
   and creation_date > (select max(creation_date) from {{ this }})
